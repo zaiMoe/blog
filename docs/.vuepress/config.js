68dd9c68d2fc99744c2c32b891be1aae23f71c99
zaiMoe <137218137@qq.com>
2019-10-05 21:01:29 +0800

    [init]加入插件和修改滚动条样式，主题样式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,13 +7,26 @@ const {fs, path} = require('@vuepress/shared-utils');
 
 module.exports = {
   evergreen: true,
-  title: 'my blog',
-  description: 'my blog',
+  base: '/', // 根路径
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'my blog', // 标题
+      description: 'c/c++ 嵌入式 linux' // 描述
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/medium-zoom', true],
+    '@vuepress/nprogress'],
   head: [
-    ['link', {rel: 'icon', href: '/logo.png'}]
+    ['link', {rel: 'icon', href: '/logo.png'}] // logo
   ],
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    lastUpdated: '上次更新时间', // 显示更新时间
+    editLinks: true, // 显示编辑提示
+    repo: 'https://github.com/Mulander-J/Wiki1001Pro.git', // blog地址
+    repoLabel: 'GitHub', // 提示名称
     nav: [
       {text: '首页', link: '/'},
       {text: 'test 1', link: '/test1/'},
@@ -39,15 +52,18 @@ module.exports = {
         {
           title: 'test2-1',
           path: '/test2/1/',
+          collapsable: false,
           children: [
             '',
             '/test2/1/',
-            '/test2/1/t1'
+            '/test2/1/t1',
+            '/test2/1/remarkpt.md',
           ]
         },
         {
           title: 'test2-2',
           path: '/test2/2/',
+          collapsable: false,
           children: [
             '/test2/2/'
           ]
---
+++ docs/test2/1/remarkpt.md
@@ -0,0 +1,43 @@
+# markdown转PPT
+
+> [源码地址](https://gitee.com/zaiMoe/remarkpt)
+
+## 这是什么？
+
+1. 将写好的md文件快速转成网页版PPT，方便平时写分享文档后还要花费时间做ppt。
+
+2. ppt使用[remark.js](https://github.com/gnab/remark)，
+这里只是将其封装成一个工具，这样我们不用关注如何生成，直接愉快的写md就行。
+markdown和remark.js使用请参考remark.js的[wiki](https://github.com/gnab/remark/wiki/Markdown)
+
+## 如何使用
+### use
+```
+git clone
+
+yarn install
+
+npm link
+
+cd */my-project
+
+remarkpt
+
+cd dist
+remarkpt server
+```
+
+### test
+```
+npm run test
+```
+
+## 命令参数
+1. `remarkpt`命令前两个参数对应要转换的md文件和转换后生成的html文件。
+
+2. -o: `output`, 输出目录， 默认是`dist`
+
+3. --css: 自定义样式表，因为是页面，你可以修改原本默认的样式 会复制到`输出目录/static/`
+
+## TODO
+1. 完善样式和添加更多的主题
